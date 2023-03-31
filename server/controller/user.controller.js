6358af1b457240c2f276bc66559399ddb4367c3b
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-03-31 09:37:38 +0530

    added

---
+++ server/controller/user.controller.js
@@ -0,0 +1,157 @@
+const { hashPassword, comparePassword } = require("../helpers/authhelper");
+const JWT=require("jsonwebtoken");
+
+const User=require("../model/user.model");
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+exports.registerUser=async(req,res)=>{
+    try {
+        const { name, email, password, gender, number } = req.body;
+        //validations
+        if (!name) {
+          return res.send({ error: "Name is Required" });
+        }
+        if (!email) {
+          return res.send({ message: "Email is Required" });
+        }
+        if (!password) {
+          return res.send({ message: "Password is Required" });
+        }
+        if (!gender) {
+          return res.send({ message: "gender no is Required" });
+        }
+        if (!number) {
+          return res.send({ message: "mobile no. is Required" });
+        }
+        
+        //check user
+        const exisitingUser = await User.findOne({ email });
+        //exisiting user
+        if (exisitingUser) {
+          return res.status(200).send({
+            success: false,
+            message: "Already Register please login",
+          });
+        }
+        //register user
+        const hashedPassword = await hashPassword(password);
+        //save
+        const user = await new User({
+          name,
+          email,
+          gender,
+          number,
+          password: hashedPassword,
+        }).save();
+    
+        res.status(201).send({
+          success: true,
+          message: "User Register Successfully",
+          user,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({
+          success: false,
+          message: "Errro in Registeration",
+          error,
+        });
+      }
+}
+
+exports.loginUser=async(req,res)=>{
+    try {
+        const { email, password } = req.body;
+        //validation
+        if (!email || !password) {
+          return res.status(404).send({
+            success: false,
+            message: "Invalid email or password",
+          });
+        }
+        //check user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          return res.status(404).send({
+            success: false,
+            message: "Email is not registerd",
+          });
+        }
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+          return res.status(200).send({
+            success: false,
+            message: "Invalid Password",
+          });
+        }
+        //token
+        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+          expiresIn: "7d",
+        });
+        res.status(200).send({
+          success: true,
+          message: "login successfully",
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            gender: user.gender,
+           number:user.number,
+            role: user.role,
+          },
+          token,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({
+          success: false,
+          message: "Error in login",
+          error,
+        });
+      }
+}
+
+// {===========================Get all users(admin)==============================}
+exports.getAllUser = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+//{===================== Get single user (admin)=====================}
+exports.getSingleUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// // {==========================Delete User --Admin=========================}
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`, 400)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
+});
+
+
+
---
+++ server/index.js
@@ -0,0 +1,25 @@
+
+
+const express= require("express");
+const connection  = require("./config/db");
+const orderRoute = require("./routes/orderRoute");
+const productRoute = require("./routes/productRoute");
+const userRouter = require("./routes/user.route");
+const app=express();
+require("dotenv").config();
+app.use(express.json())
+const port= process.env.PORT
+
+
+app.get("/",(req,res)=>{
+    res.send("home page")
+})
+
+app.use("/user",userRouter);
+app.use("/products",productRoute)
+app.use("/order",orderRoute)
+
+connection();
+app.listen(port,()=>{
+    console.log(`port is running at ${port}`)
+})
