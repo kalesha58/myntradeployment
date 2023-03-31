0677cc45073020a4859ac128d59698fe41504d04
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-03-31 10:31:58 +0530

    added

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,38 +1,68 @@
-const JWT=require("jsonwebtoken");
-const User=require("../model/user.model");
+const ErrorHander = require("../utils/errorhandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+const jwt = require("jsonwebtoken");
+const User = require("../model/user.model");
 
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new ErrorHander("Please Login to access this resource", 401));
+  }
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
+
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHander(
+          `Role: ${req.user.role} is not allowed to access this resouce `,
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
 //Protected Routes token base
 exports.requireSignIn = async (req, res, next) => {
-    try {
-      const decode = JWT.verify(
-        req.headers.authorization,
-        process.env.JWT_SECRET
-      );
-      req.user = decode;
-      next();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const decode = JWT.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  //admin acceess
+//admin acceess
 exports.isAdmin = async (req, res, next) => {
-    try {
-      const user = await User.findById(req.user._id);
-      if (user.role !== 1) {
-        return res.status(401).send({
-          success: false,
-          message: "UnAuthorized Access",
-        });
-      } else {
-        next();
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(401).send({
+  try {
+    const user = await User.findById(req.user._id);
+    if (user.role !== 1) {
+      return res.status(401).send({
         success: false,
-        error,
-        message: "Error in admin middelware",
+        message: "UnAuthorized Access",
       });
+    } else {
+      next();
     }
-  };
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      success: false,
+      error,
+      message: "Error in admin middelware",
+    });
+  }
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,7 +2,7 @@
 const express=require("express");
 
 const { registerUser, loginUser, getAllUser, getSingleUser, deleteUser } = require("../controller/user.controller");
-const { requireSignIn, isAdmin } = require("../middleware/authMiddleware");
+const { requireSignIn, isAdmin, isAuthenticatedUser, authorizeRoles } = require("../middleware/authMiddleware");
 
 
 const userRouter=express.Router();
@@ -10,6 +10,6 @@ const userRouter=express.Router();
 userRouter.post("/register",registerUser)
 userRouter.post("/login",loginUser)
 userRouter.get("/:id", requireSignIn , isAdmin, getSingleUser)
-userRouter.get("/allusers",requireSignIn , isAdmin,getAllUser)
+userRouter.get("/allusers",isAuthenticatedUser, authorizeRoles("admin"), getAllUser)
 userRouter.delete("/:id",requireSignIn , isAdmin,deleteUser)
 module.exports=userRouter
